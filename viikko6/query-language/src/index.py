750655238e52afbe53227321594c6e724518e2a4
Matti Lavikainen <matti.lavikainen@helsinki.fi>
2023-11-30 17:56:31 +0200

    Parannettu kyselykieli parts I and II

--- viikko6/query-language/src/index.py
+++ viikko6/query-language/src/index.py
@@ -1,24 +1,29 @@
 from statistics import Statistics
 from player_reader import PlayerReader
-from matchers import And, HasAtLeast, PlaysIn, Not, HasFewerThan, All, Or 
+from matchers import And, HasAtLeast, PlaysIn, Not, HasFewerThan, All, Or
+from querybuilder import QueryBuilder
 
 def main():
     url = "https://studies.cs.helsinki.fi/nhlstats/2022-23/players.txt"
     reader = PlayerReader(url)
     stats = Statistics(reader)
 
-    matcher = And(
-    HasAtLeast(70, "points"),
-    Or(
-        PlaysIn("NYR"),
-        PlaysIn("FLA"),
-        PlaysIn("BOS")
+    query = QueryBuilder()
+    matcher = (
+  query
+    .oneOf(
+      query.playsIn("PHI")
+          .hasAtLeast(10, "assists")
+          .hasFewerThan(5, "goals")
+          .build(),
+      query.playsIn("EDM")
+          .hasAtLeast(50, "points")
+          .build()
     )
+    .build()
 )
-
     for player in stats.matches(matcher):
         print(player)
-
 
 if __name__ == "__main__":
     main()
